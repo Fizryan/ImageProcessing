458ff987b4badc6cd557ea247f193733e253a8e1
Fizryan <tdark9362@gmail.com>
2025-09-01 22:27:22 +0700

    Update .gitignore and Logging_Config.py for improved logging and file management

--- program/Logging_Config.py
+++ program/Logging_Config.py
@@ -2,42 +2,40 @@
 # This module configures the logging settings for the application.
 
 LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s - (%(module)s:%(lineno)d)'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s - (%(module)s:%(lineno)d)"
         },
     },
-    
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard',
-            'level': 'INFO',
-            'stream': 'ext://sys.stdout',
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "level": "INFO",
+            "stream": "ext://sys.stdout",
         },
-        'file': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'standard',
-            'filename': 'logs/app.log',
-            'maxBytes': 10485760,
-            'backupCount': 1,
-            'encoding': 'utf8',
-            'level': 'DEBUG',
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "standard",
+            "filename": "logs/app.log",
+            "maxBytes": 10485760,
+            "backupCount": 1,
+            "encoding": "utf8",
+            "level": "DEBUG",
+            "delay": True,
         },
     },
-    
-    'loggers': {
-        '': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
+    "loggers": {
+        "": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
         },
-        'another_library': {
-            'handlers': ['file'],
-            'level': 'WARNING',
-            'propagate': False,
-        }
-    }
+        "another_library": {
+            "handlers": ["file"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+    },
 }
